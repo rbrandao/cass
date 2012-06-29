29f5f271893090535119c3019839cf9bfbcc9497
wsn <wsn@wsn-laptop.(none)>
2012-06-28 23:04:14 -0300

    adad

--- src/cass.h
+++ src/cass.h
@@ -2,34 +2,32 @@
 #define CASS_H
 
 enum {
-	CASS_ID = 100,
-	HOPS_MAX_NUMBER = 4,
+	CASS_ID = 100, 
+	HOPS_MAX_NUMBER = 3,
+
 	TIMER_PERIOD = 2000,
 	DELAY_TIME = 1050,
 
-	//Tempo máximo de espera pela eleição
-	ELECTION_TIMEOUT = 10000,
 	
-	//Node que iniciará a primeira eleição
-	ELECTION_STARTER = 1,
+	LIDER_ELECTION_ID = 200,
+	LIDER_RESPONSE_ID = 201,
+	LIDER_VICTORY_ID = 202,
 	
-	//Identificadores das mensagens de eleição de líder
-	ELECTION_MSG_ID = 200,
-	RESPONSE_MSG_ID = 201,
-	VICTORY_MSG_ID = 202
+	MAX_MESSAGE_ID_BUFFER_LEN = 5,
 };
 
 // broadcast
 typedef nx_struct cass {
 	nx_uint16_t serverID; // ID do Servidor.
 	nx_uint16_t clientID; // ID do Cliente.
-	nx_uint16_t messageID; //id da mensagem.
-	nx_uint16_t electionMsgID; //id da mensagem de eleição de líder.
-	nx_uint16_t groupID; //grupo que receberá a mensagem.
-	nx_uint8_t hops; // número de saltos da mensagem.
-	nx_uint16_t value; //o valor da mensagem.
+	nx_uint16_t messageID; // id da mensagem.
+
+	nx_uint16_t groupID; // grupo que receberá a mensagem.
+	nx_uint16_t hops; // número de saltos da mensagem.
+	
+	nx_uint16_t value; // valor da mensagem.
 } cassMsg_t;
 
 
 
-#endif // CASS_H
+#endif /* CASS_H */
